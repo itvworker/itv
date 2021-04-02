81d8ee589355e50dbc4c0a1be77f706f15dfa1bc
xiekx2 <xiekx2@midea.com>
2021-04-02 18:23:28 +0800

    ui

--- src/itv-components/progress/demo.vue
+++ src/itv-components/progress/demo.vue
@@ -3,7 +3,7 @@
         <itv-header>Progress</itv-header>
         <itv-main>
             <div class="h10"></div>
-            <itv-progress class="progress" v-model="percent" strokeColor="rgba(255,15,16,1)"/>
+            <itv-progress class="progress" v-model="percent" strokeColor="rgba(0, 0, 0, 0.45)"/>
 
             <itv-progress class="progress" v-model="percent" anticlockwise :bgPercent="90"  :strokeWidth="10" :bgStrokeWidth="50" strokeLinecap="butt"  strokeColor="rgba(255,15,16,1)"/>
         </itv-main>
