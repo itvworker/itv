90eb9a73c16dcb57313bfc916ad33ec57d1184a7
ning <ning@ningdeMacBook-Pro.local>
2021-08-01 18:38:54 +0800

    picker

---
+++ src/components/picker/index.js
@@ -0,0 +1,60 @@
+Component({
+    properties: {
+        listData: {
+            type: Array,
+            required: true
+        },
+        defaultValue: {
+            type: String | Number
+        },
+
+        keyIndex: {
+            type: Number,
+            value: 0
+        },
+        isUpdate: {
+            type: Boolean,
+            value: false
+        },
+        lastChange: {
+            type: Boolean,
+            value: false
+        },
+        word: {
+            type: String,
+            value: '{value}'
+        },
+        //是否取整
+        isInteger: {
+           type: Boolean,
+           value: false 
+        },
+        isLoop: {
+            type: Boolean,
+            value: false
+        },
+        rows: {
+            type: Number,
+            value: 5
+        },
+        pickerIndex: {
+            type: Number,
+            value: null
+        },
+        clearMask: {
+            type: Boolean,
+            value: false
+        }
+    },
+    data: {
+        reload: 0,
+    },
+    methods: {
+        onLoad: function() {
+            this.setData({
+                reload: this.data.reload+1
+            })
+        }
+    }
+  
+})
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,31 +2,33 @@
 // 获取应用实例
 const app = getApp()
 Page({
-  data: {
-    motto: 10,
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    data: {
+        motto: 10,
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        canIUseGetUserProfile: false,
+        canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+        listData:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        defaultValue: 5
   },
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
-      url: '../logs/logs'
+        url: '../logs/logs'
     })
   },
   onLoad() {
     if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
+        this.setData({
+            canIUseGetUserProfile: true
+        })
     }
 
     
   },
   getUserInfo() {
-    return 'user'
+        return 'user'
   }
     
 })
